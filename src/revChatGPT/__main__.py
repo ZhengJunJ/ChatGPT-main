df5c1410dbf1f64e564d3c286c40ea1577034134
zhengjunjie502 <zhengjunjie502@hellobike.com>
2022-12-12 17:51:47 +0800

    post get请求的支持

--- src/revChatGPT/__main__.py
+++ src/revChatGPT/__main__.py
@@ -178,8 +178,8 @@ def main():
 
 
 config = {
-    "session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..sg9Z_mBu9Isrl1za.Png2n2LlQeMVQGZOEMNsACO6O-GeJuq0Fb_UjcanlF89Pdm9SYzUVD9g50IIctH6Q4Xz1pvEJkbzu8ImI_woXqm-PH2gP9Sgw4XRElckuVapl6Kf8D3EpMNsPLPY2Br0DBRMJDQgxYOJYW6S-jjYDDoJXNgep1mSSvunX7vQyDimoA7qkSdXxJWHq0eAWZBBudTLFAGk8XmNkuTrSfX8_ZH5IaoUT6TiBbbSGFkvX5GY-EBmnTQIeMZGKElu4pMGpoKconPL8zgSfPOIkcGVBc0DNzazvhVNjMtZy_4RjuT0WuufxHjNYLoAT2UIAokOOmLGS6IzP3W3cg5d01jSjew9iQSAhmON1qSg1Zwrah-ndrfeZDyadvBDyHCWwWG0DEsYBnp9zXXlk-7mEexnzHVgf4TwIC06BAWDN4yL2bwtAFAqNjtmyICo9AS9kYxP1Sbu8H2hoAAsiBa4t6-m7_DnRRA92F6uYT8TDRhN8mOhEdb9XhrmOhTbaalUe-YFMPrLBbCu92bU05_y_Ph_2jCfBXDIsn99dHu29-HKXunOp2uvZK50X1h9z_Cv7jLlLrH7A8MEOpZBmGGell37cpeUwTN1-11mqgopEvmDKIccTHt_cfnaIl3CJ4PHtvVvA4a_wO6I3ElUfpj7ojayYaAh7uoBtDz2BWhkOnNc4pVLYgpdRShwE2ELg87Gu1L06p0Ioo8uMUZijndiv2hx6ZK1w9P3vvS-nT-uRtVW6CtjjCfNbUoftmu6fgLw_psEoVOCWrgTpEhDZzgSMY1dKhx-B4hK9X-eI-ecxx-yQQ0ABYehnwy7g64jOKVTl5gISX21MPdNJFKSB4T44RauIteqVf12v5spBv2AAWTi7enFwzjUhMiYW4CyxChT9Y9puOFBtCSlOCZO9T7YXaWeqTBfzrKmBGtrl758_MLRcbDwPFGvI52gHi9POD5kg5LUn4qxSHT-E7uAKivNLf4ZrFSbDHclKoML901rbr7cFYRV5y7sfy-G8PKJAf3VNT2xYa-EFZSScl6aBDwPoBHab5riBXGy8y4am_1La53ZJz0SgifFLE696weakCKtWmGZRIV6VFUGry0153G90BKP8F5mMhDDgwFmphZyJA79Bh_GedNvENZm7rrPLqPjonnrUqRq-qR6ZkTU-JVGOcpz-9VxW7fZDptNuSQbjOOFNlWJGWCYIFt7slMVgeJbjUW8bESXYEgvj-Uey7zMHHoDd33Vlupz9PQpkW2UQNibA01QMlfUnWEGBDYmDvWhMQdcoHvLnaNOFu_q70x9vV3JawyLl100pOWvZH2isDHDtbQrD9_NXIBu8OdPnzwT_zdbwTiROjDVLtJLoRDSzRT91E6PhzMy5n8kbS2psiHH9d2cFxzpS7wuprg-jiZLnCha4rAa5K1eZt4kZKyiER7Txf97FWKnAnTmrfaftw4tuQheuAOFIAYhhyX1Z-kFd3Q0d3o_ELkE9N8JnRk_X1s3hWVDR92nl60UoifSM7xuzSd6VmWK7f_yMw9vkoHXFRa7RxICDl2_J4U-NONdk7Ncu3qHY0ka2T25A5Qe-ccC3G6X7hl9PdjAnxHldlj4GKC9MZ6GmNRQ08o-yvF_GdqfkOZcGwiO1oSsV3swHWbsPU3deKlA3j2XNs2-RNjnuTjHhTZbvhW_MPACLnn-ENILxnIwexyvwh6DbjaisvRth8XKUvqIxmNtuTCSllJvqFUxddOViEaZut2exceHotxGqFITtOhimO2xFLMx_lX3UxQrdUfNAAdGhV9PSgEH2JRzYfVeXttVFbu3XlgrO0vh6OmmaxqQvvb_rsSj7pbnwHsUEowJBeZbSxtPDJj9apBELosx7l41rQpHYIlJ3UktPKOShWrb1TEEThvfwJo4-uJQvp2pGHNS5r88S-8ZhQPaloAdgFik1K49y55lLOZ59jrdw1t-Ix5O1GnegV1d58JkLVGmsVNILTeb8FvSctDdsRdMnMgrWBC10O71xxvFmgB4rtacnNQmSyGlIg2-WQ2wShNATxVBvDu1jhuqaFRM8UKY28UTcAq7BWGrZwEekjkstd698KTS1EZA5UDJT66uWbMalK5WU06-cy1_ScGyP_60Lku9aA_42R4MNKtLRZTaACsT8YgE9Mxhs6g3Su28wkRStg8by4xmbJZliNI6mWtpA-YYI3Jfj-BDTSuyhFiyv_BcNYyh7-Ahffas3KpPKjKJqfvkD6eoG3m91ADS7FISGfX3jz3W-7io_L49cjG-NIFvvGY5Ah2B2-hMRKCn0Pi6fTvNXD07bXvTQNsL0zKkcYTD31ldeTiZsLWRmJqwsa4jqIJHXLBRy0rD46XHxulPj3ix4wQ.joY7YtZTbMcoQGeMW9k2lA",
-    "cf_clearance": "ys5CMGahCEsrR8bBYVKYG6buvJGdwWCkePhYH.6NTeQ-1670830003-0-160",
+    "session_token": "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..Iu0M7if5g7ufUVV6.dOUKL-FK6N_cBlGw4J6yBV_DNkSceyxuAN9InommWUJRd_Pd7aTQl9HOKDx31QnKahDLNlqEo9BTDAQLUMlf3FV-HL8yzLeyQoaXIDeBDBtXZi87Wz9MmEUYljAtmmp1yvJ6GKDWLBjGoFgq7STNfNGn-9Vw9pDSiLQFULInjjoqeiSBDrzxozx6HVHg2jODGXCG3_A9NIxVGWmvHyAlJQgpyjkFXgAAyCxgQA0iKWVQqg7Yi8vGzBLDyQ6UKs7C6tU3BzvK9I-U83ImoymO0980t0fqoTvVzz-zpU2fTEfk5No-eN0TyXkh1YLcaXcEw2esxGmcJPlNR9PgzGRKaiKkfbgazu0sIBVBdMgpj6VRbRTC9Fg-DJgAqb4aiQUmZ0TllQEMQDKKVNf6Jj3H-rFbAnZjI_FhKiYpDb1CEtke5VXgFMHRmAxEe36tiJNlQwil5CLeQQXjET1ydUrB7_kXFVRQam2vwF6zi3nuWrwv5gF7OfrlHn7J6XTkvduBTLw0ECTpNLfgi4vaUfKVT4_0J4Z-pX5itKNgVYwlVJexY8OJ1ebHu3R5rWqI2RNpbfcXGjhawu4fXSUwkWyHIMfZZdpQldDYkEuKKH_648eBNYynRrAn1fqwRYsKOBSAT4xxl4r4C9QMk9acQc9RtViKITnrPTbCdb0fYGyV2qwQ3Vc5c7ih7v66HTA-ixGTZrMyHECIytsKLHZcoJUiBtin6cE_r7N70gVGpOzKA_gkviZjVZkhi7fPMVERle9EqLUWWvC2ehBFSg_n8JOS1b1AWBe12QCDHmkDSLHd1KQKW4-zFPzmZSbfH8fUGjU5c0Sh3OZAMc-HcFa4JF9KcmqRqjpBV4-CCPS7yMj_ndzJCyDXOunsy_KCXaiGfKZfGWd9SovoOYaANM48KbNKBS44FCMxkztnyQXyHyQvxF7vMYCQUxP1CMmxYsWCvM6qAQmmqnoMiBEHDLGNjaDC5as_MvepqHo94aZW_CXUNBDppDEY0D8lFBm3MO-UtVUnmUlG71udQJ-RFVdgKcJ5w9EwHNn_3kPIyWdIVhub9DPJfNk3vkjCIaH5KkTcIWIIZukKsmoQvDKm36nvzg0WomvQUktKde52S47_32ebIwNLh6tfEX9QGQR4BqxSDO4v_BTLgBiK126i-_E_CnrzPE2aozI86QUWVs7-qGv4N5LJdI1Af4XJ5_cv7cW5tZ1217MwuCnD60_W8rCL2hxyOv_Gcs1buN1oMDEc43Xv2M1Zj87LXmnvxPXl10JxVl7E9miUfMKzCf_UsFvYX2zvqFYXuT1gonS3PHjjYQsi9fn61ExZNzooCzJdHFMXTVclgJa1uWJcrOSE9iN3tebJft3oX7bcQaDVU2E7wByKiglEdJR98NfxaiyuvpghJeM0ufNfnM66dYl9SwF0sJjYGDbn9uz8HMEyg_QG92e-Mqv7H2iW7T1eZmEfLTbfLxzuneGOiAEnEoyfwiqHsxDz4UjjBd2PUSC4KEq-VhRxguWPfheYxxmPXzCsxBG38AsmPOCdjIMNxuO1UmR2dRYPf6FuqyYqcxpynrAbPi0e49XMdbiVA5j0sTW1R8tn36t8bKpdi5xz-j0h6q_vtcgrDFhfhJm290a0QxLPx9tdpD0wH1U7Y0jeJZ7GuSkw3vdSzorypDHWA73C9AV1zKl0e1iyFN8yTykEfQIBlp8g7yuaDhb6iBwIfcP6HRflYZLAPbtvhm5g6FKn9wx0Fub8cI06xRcBQwDbTAUhtFYZax1PAw-Ied0E-DH0sBZRNtvOqdehr9mEdybyZjdNmdmN2-q_hyMZLjQYe1u1Rs2sgUSQP-Qf2NuuynYf3mwSlhSU93N7WWZwIqkb7mmf_flUMOkcaIcmshMRhdQ3g_EcKz5Vg6f330vqCqIUEAVgqkDzMMJTmL61hrvZlhj4M1Kp_4WdmJ0lQ2AIi5n-KkvwQe8xDwG2MN690ivZ3ibh9sgqFzR6XRVXuFFmVzSsZ_vfNcgqk2JZJqGNiXwj3tu1Uqvqr96bHDbiQKXln9VXdAvof-haDAq9P0oalL9w6PH8qePyLDrWC_S-rzjllyGmj7p4nMH2mk0KmGQneUejYo_RP8_B6IZ7YBYcriUYxX0Yqlui5LOcYZVHy6gVBAPaGOBPTlvU9-DHuevc4CoUl62iEnGOrTX-eaK9J9WE5LIiKWRpwfL9pbVW9faVu3G1jtxs7b3FmU9eFqJBFxI9qRguevnfoFwWiyccNhir8Ibr7XxsU_sbV_qz8AvSJKfKQlJdGYSjx64HopYURAy-bJOkub1ZL5Z_0MIv3irPvXPChWjl4AiraVW3jCfT1W7Wg2_SXJOy9xUA45A.6tLHED0cUfNByRTwoUWzaw",
+    "cf_clearance": "qX_ViEpb0v_nlDVGmsguPDADH0he7x5rBAuALAJ3vwI-1670837472-0-160",
     "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 }
 
@@ -191,19 +191,29 @@ class MainHandler(tornado.web.RequestHandler):
     def __init__(self, *argc, **argkw):
         super(MainHandler, self).__init__(*argc, **argkw)
 
+    def options(self):
+        # 跨域options请求 直接返回ok
+        self.set_status(204)
+        self.finish()
+
     # 解决跨域问题
     def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")  # 这个地方可以写域名
+        self.set_header("Access-Control-Allow-Origin", self.request.headers.get("Origin", "*"))
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
         self.set_header("Access-Control-Max-Age", 1000)
         self.set_header("Content-type", "application/json")
 
     def get(self):
+        # get 请求
         question = self.get_argument('question')
         response = hello_chatbot.get_chat_response(str(question), output="stream")
+        result = None
         for res in response:
-            self.write("response=" + str(res))
+            result = res
+        result_json = json.dumps(result)
+        # print(result_json)
+        self.write(result_json)
 
     def post(self):
         # post请求
@@ -212,8 +222,12 @@ class MainHandler(tornado.web.RequestHandler):
         body_json = json.loads(body_decode)
         question = body_json.get("question")
         response = hello_chatbot.get_chat_response(str(question), output="stream")
+        result = None
         for res in response:
-            self.write("response=" + str(res))
+            result = res
+        result_json = json.dumps(result)
+        # print(result_json)
+        self.write(result_json)
 
 
 application = tornado.web.Application([(r"/chat", MainHandler), ])
